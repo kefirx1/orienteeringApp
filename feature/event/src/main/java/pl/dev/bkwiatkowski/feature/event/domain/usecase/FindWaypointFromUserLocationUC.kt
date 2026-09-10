7d3fed1a6f40c38eceea8fea457b5380f6a53480
Blazej Kwiatkowski <blazejkwiatkowski02@gmail.com>
2026-09-10 17:16:29 +0200

    New location search

--- feature/event/src/main/java/pl/dev/bkwiatkowski/feature/event/domain/usecase/FindWaypointFromUserLocationUC.kt
+++ feature/event/src/main/java/pl/dev/bkwiatkowski/feature/event/domain/usecase/FindWaypointFromUserLocationUC.kt
@@ -7,7 +7,6 @@ import pl.dev.bkwiatkowski.common.core.usecase.EitherUseCase
 import pl.dev.bkwiatkowski.common.core.usecase.UseCase
 import pl.dev.bkwiatkowski.common.core.usecase.either
 import pl.dev.bkwiatkowski.feature.event.domain.model.MapWaypoint
-import kotlin.math.round
 
 interface FindWaypointFromUserLocationUC : EitherUseCase<FindWaypointFromUserLocationUC.Params, MapWaypoint?> {
   data class Params(
@@ -17,17 +16,19 @@ interface FindWaypointFromUserLocationUC : EitherUseCase<FindWaypointFromUserLoc
 }
 
 class FindWaypointFromUserLocationUCImpl : FindWaypointFromUserLocationUC {
+  companion object {
+    private const val WAYPOINT_RADIUS_METERS = 15f
+  }
+
   override suspend fun invoke(params: FindWaypointFromUserLocationUC.Params): Either<DomainError, MapWaypoint?> = either {
-    fun Double.round4(): Double = round(this * 10000.0) / 10000.0
-
-    val roundedUserLat = params.currentLocation.latitude.round4()
-    val roundedUserLng = params.currentLocation.longitude.round4()
-
     params.waypoints.find { waypoint ->
-      val wpLat = waypoint.position.latitude.round4()
-      val wpLng = waypoint.position.longitude.round4()
+      val waypointLocation = Location("").apply {
+        latitude = waypoint.position.latitude
+        longitude = waypoint.position.longitude
+      }
 
-      return@find (wpLat == roundedUserLat && wpLng == roundedUserLng)
+      val distanceInMeters = params.currentLocation.distanceTo(waypointLocation)
+      distanceInMeters <= WAYPOINT_RADIUS_METERS
     }
   }
 }
